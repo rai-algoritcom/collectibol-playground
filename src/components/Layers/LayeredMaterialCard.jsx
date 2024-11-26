a0c5bff33846d5bee5037496f7486f989ed91317
Raimon <raimon@MBP-de-Raimon.localdomain>
2024-11-26 15:41:28 +0100

    added Troika text

--- src/components/Layers/LayeredMaterialCard.jsx
+++ src/components/Layers/LayeredMaterialCard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import * as THREE from 'three';
 import { button, useControls } from "leva";
 import { useFrame, useThree } from "@react-three/fiber";
-import { useTexture } from "@react-three/drei";
+import { Text, useTexture } from "@react-three/drei";
 import gsap from "gsap";
 
 
@@ -66,6 +66,7 @@ import {
 } from "../../utils";
 
 import { downloadJSON, takeScreenshot } from "../../utils/helpers";
+import TroikaTextLayer from "./TroikaTextLayer";
 
 
 
@@ -323,6 +324,16 @@ export default function LayeredMaterialCard({ textures, texturePaths }) {
         useParticles: { value: false, label: '[!] Particles Fx' },
         useBlobs: { value: false, label: 'Blobs Fx' },
         useGrass: { value: false, label: 'Grass Fx' }
+    })
+
+
+    const { color, fontSize, maxWidth, lineHeight, letterSpacing, content } = useControls('Text Overlay', {
+        color: { value: "#ffffff", label: 'Color' },
+        fontSize: { value: .1, min: 0, max: 1, label: 'Font Size' },
+        maxWidth: { value: 1, min: 1, max: 5, label: 'Max Width' },
+        lineHeight: { value: 0.75, min: 0.1, max: 10 , label: 'Line Height' },
+        letterSpacing: { value: -0.08, min: -0.5, max: 1, label: 'Letter Spacing' },
+        content: { value: 'LOREM IPSUM DOLOR SIT AMET', label: 'Content'}
     })
 
 
@@ -838,6 +849,21 @@ export default function LayeredMaterialCard({ textures, texturePaths }) {
                     />
                 </mesh>
             )}
+
+            <Text
+                color={color}
+                fontSize={fontSize}
+                maxWidth={maxWidth}
+                lineHeight={lineHeight}
+                letterSpacing={letterSpacing}
+                textAlign={'center'}
+                font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+                anchorX="center"
+                anchorY="top"
+                position={[0, -1, .15]}
+            >
+                {content}
+            </Text>
         </>
     )
 }
