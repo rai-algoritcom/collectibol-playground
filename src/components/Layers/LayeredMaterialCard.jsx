99cc2e4d78bbb85cc3513b04b8e30ad77f11afdb
Raimon <raimon@MacBook-Pro-de-Raimon.local>
2024-12-03 12:41:07 +0100

    added Footer

--- src/components/Layers/LayeredMaterialCard.jsx
+++ src/components/Layers/LayeredMaterialCard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import * as THREE from 'three';
 import { button, useControls } from "leva";
 import { useFrame, useThree } from "@react-three/fiber";
-import { Html, Text } from "@react-three/drei";
+import { Text } from "@react-three/drei";
 import gsap from "gsap";
 
 
@@ -70,7 +70,8 @@ import {
 import { downloadJSON, normalizeAngle, takeScreenshot } from "../../utils/helpers";
 
 import Stats from "three/examples/jsm/libs/stats.module.js";
-import SkillLabel from "./SkillLabel";
+import FooterCard from "./FooterCard";
+import SkillsCard from "./SkillsCard";
 
 
 
@@ -989,40 +990,12 @@ export default function LayeredMaterialCard({ textures, texturePaths, layoutColo
                 {textContent}
             </Text>}
 
-
-            <Html
-                position={[0, -.8, 0.1]}
-                transform
-                scale={1}
-                distanceFactor={2}
-                distance
-                side={THREE.FrontSide}
-            >
-                <div style={{ padding: '1rem', width: '360px', height: 'max-content', border: '1px solid transparent'}}>
-
-                    <SkillLabel bgColor={'rgba(143, 50, 0, 0.5)'}>
-                        <span>Lamine Yamal, la Revelación</span>
-                    </SkillLabel>
-                    <SkillLabel bgColor={'rgba(0, 0, 0, 0.5)'}>
-                            <img src="/icons/ball.png" style={{ width: '30px', height: '30px'}} />
-                            <span>Rematador estrella</span>
-                            <div style={{ display: 'flex', alignItems: 'center' }}>
-                                <img src="/icons/energy.png" style={{ width: '20px', height: '20px'}} />
-                                <span>+2</span>
-                            </div>
-                    </SkillLabel>
-                    <SkillLabel bgColor={'rgba(0, 0, 0, 0.5)'}>
-                            <img src="/icons/player.png" style={{ width: '40px', height: '40px'}} />
-                            <span>Centrocampista táctico</span>
-                            <div style={{ display: 'flex', alignItems: 'center' }}>
-                                <img src="/icons/energy.png" style={{ width: '20px', height: '20px'}} />
-                                <span>+1</span>
-                            </div>
-                    </SkillLabel>
-               
-                </div>
-            </Html>
             
+            <SkillsCard />
+
+            <FooterCard />
+
+      
         </group>
     )
 }
