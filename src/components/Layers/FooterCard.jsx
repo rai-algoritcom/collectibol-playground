f917f7585d8d1651058310e5dafa46b44430af6b
Raimon <raimon@MacBook-Pro-de-Raimon.local>
2024-12-03 12:41:07 +0100

    added Footer

---
+++ src/components/Layers/FooterCard.jsx
@@ -0,0 +1,27 @@
+import { Html } from "@react-three/drei";
+
+export default function FooterCard() {
+    return (
+        <Html
+            position={[0, -1.35, 0.005]}
+            transform
+            scale={1}
+            distanceFactor={2}
+            distance
+        >
+            <div style={{ display: 'flex', width: '370px', justifyContent: 'space-between', alignItems: 'center',  marginTop: '.25rem' }}>
+                <div style={{ color: 'white', fontFamily: "'Inter Tight', sans-serif"}}>
+                    <span style={{fontSize: '9px', fontWeight: 'bold', color: 'gainsboro'}}>FC BARCELONA</span>
+                    <span style={{fontSize: '8px', fontWeight: 'bold', display: 'block', color: 'gainsboro'}}>&copy; 2025 Collectibol</span>
+                </div>
+                <div style={{ marginLeft: '-1rem'  }}>
+                    <img src="/icons/rare.png" style={{ width: '60px', height: '60px'}} />
+                </div>
+                <div style={{ color: 'white', fontFamily: "'Inter Tight', sans-serif" }}>
+                    <span style={{fontSize: '18px', fontWeight: 'bold', color: 'gainsboro'}}>EX 3</span>
+                    <span style={{fontSize: '8px', fontWeight: 'bold', display: 'block', color: 'gainsboro'}}>25 18 A0 45</span>
+                </div>
+            </div>
+        </Html>
+    )
+}
---
+++ src/components/Layers/SkillsCard.jsx
@@ -0,0 +1,39 @@
+import { Html } from "@react-three/drei";
+import SkillLabel from "./SkillLabel";
+
+
+export default function SkillsCard() {
+    return (
+        <Html
+                position={[0, -.8, 0.1]}
+                transform
+                scale={1}
+                distanceFactor={2}
+                distance
+            >
+                <div style={{ padding: '1rem', width: '360px', height: 'max-content', border: '1px solid transparent'}}>
+
+                    <SkillLabel bgColor={'rgba(143, 50, 0, 0.5)'}>
+                        <span>Lamine Yamal, la Revelación</span>
+                    </SkillLabel>
+                    <SkillLabel bgColor={'rgba(0, 0, 0, 0.5)'}>
+                            <img src="/icons/ball.png" style={{ width: '30px', height: '30px'}} />
+                            <span>Rematador estrella</span>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <img src="/icons/energy.png" style={{ width: '20px', height: '20px'}} />
+                                <span>+2</span>
+                            </div>
+                    </SkillLabel>
+                    <SkillLabel bgColor={'rgba(0, 0, 0, 0.5)'}>
+                            <img src="/icons/player.png" style={{ width: '40px', height: '40px'}} />
+                            <span>Centrocampista táctico</span>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <img src="/icons/energy.png" style={{ width: '20px', height: '20px'}} />
+                                <span>+1</span>
+                            </div>
+                    </SkillLabel>
+               
+                </div>
+        </Html>
+    )
+}
